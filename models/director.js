1883e0b8107a2741564df8cf32117eb10dac4285
Paul Petrov <paul@bargamut.ru>
2020-10-22 18:45:02 +0300

    Add Mongoose Schemas for MongoDB

---
+++ models/director.js
@@ -0,0 +1,11 @@
+// Mogoose SSChema for MongoDB
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const directorSchema = new Schema({
+	name: String,
+	age: Number,
+});
+
+module.exports = mongoose.model(`Director`, directorSchema);
