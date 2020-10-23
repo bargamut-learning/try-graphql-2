0e7f5785bf89bdb5d3dfb7dad0764407d363797b
Paul Petrov <paul@bargamut.ru>
2020-10-23 19:22:13 +0300

    Fix order middlewares for express

--- server/server/app.js
+++ server/server/app.js
@@ -12,12 +12,12 @@ const { DB_HOST, DB_USER, DB_PASS } = process.env;
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/graphql-tutorial?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
+
 app.use(`/graphql`, graphqlHTTP({
 	schema,
 	graphiql: true,
 }));
-
-app.use(cors());
 
 const dbConnection = mongoose.connection;
 
