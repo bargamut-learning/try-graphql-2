c3e49d557d4c08f1f49111788802ca464315cc63
Paul Petrov <paul@bargamut.ru>
2020-10-26 14:09:06 +0300

    Refactoring: compose add & edit mutations

--- application/src/components/DirectorsForm/DirectorsFormHoc.js
+++ application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -7,34 +7,36 @@ import { directorQuery } from '../DirectorsTable/queries';
 
 import { styles } from './styles';
 
-const withGraphqlAdd = graphql(addDirectorMutation, {
-	// props пойдут в сам компонент
-	props: ({ mutate }) => ({
-		/**
-		 * @description Передаёт данные режиссёра аргументами в мутацию
-		 * @author Paul "Bargamut" Petrov
-		 * @date 26/10/2020
-		 * @param {*} director объект данных нового режиссёра, содержит name и age
-		 */
-		addDirector: director => mutate({
-			variables: director,
-			// Массив объектов дополнительных запросов при выполнении мутации
-			refetchQueries: [
-				{ query: directorQuery, },
-			],
+const withGraphQl = compose(
+	graphql(addDirectorMutation, {
+		// props пойдут в сам компонент
+		props: ({ mutate }) => ({
+			/**
+			 * @description Передаёт данные режиссёра аргументами в мутацию
+			 * @author Paul "Bargamut" Petrov
+			 * @date 26/10/2020
+			 * @param {*} director объект данных нового режиссёра, содержит name и age
+			 */
+			addDirector: director => mutate({
+				variables: director,
+				// Массив объектов дополнительных запросов при выполнении мутации
+				refetchQueries: [
+					{ query: directorQuery, },
+				],
+			}),
 		}),
 	}),
-});
 
-const withGraphqlUpdate = graphql(updateDirectorMutation, {
-	props: ({ mutate }) => ({
-		updateDirector: director => mutate({
-			variables: director,
-			refetchQueries: [
-				{ query: directorQuery, },
-			],
+	graphql(updateDirectorMutation, {
+		props: ({ mutate }) => ({
+			updateDirector: director => mutate({
+				variables: director,
+				refetchQueries: [
+					{ query: directorQuery, },
+				],
+			}),
 		}),
-	}),
-});
+	})
+);
 
-export default compose(withStyles(styles), withGraphqlAdd, withGraphqlUpdate);
+export default compose(withStyles(styles), withGraphQl);
--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -8,26 +8,28 @@ import { directorsQuery } from './queries';
 
 import { styles } from './styles';
 
-const withGraphqlAdd = graphql(addMovieMutation, {
-	props: ({ mutate }) => ({
-		addMovie: movie => mutate({
-			variables: movie,
-			refetchQueries: [
-				{ query: movieQuery, },
-			], 
-		})
-	}),
-})
-
-const withGraphqlUpdate = graphql(updateMovieMutation, {
-	props: ({ mutate }) => ({
-		updateMovie: movie => mutate({
-			variables: movie,
-			refetchQueries: [
-				{ query: movieQuery, },
-			],
+const withGraphQl = compose(
+	graphql(addMovieMutation, {
+		props: ({ mutate }) => ({
+			addMovie: movie => mutate({
+				variables: movie,
+				refetchQueries: [
+					{ query: movieQuery, },
+				], 
+			})
 		}),
 	}),
-});
 
-export default compose(withStyles(styles), withGraphqlAdd, withGraphqlUpdate, graphql(directorsQuery));
+	graphql(updateMovieMutation, {
+		props: ({ mutate }) => ({
+			updateMovie: movie => mutate({
+				variables: movie,
+				refetchQueries: [
+					{ query: movieQuery, },
+				],
+			}),
+		}),
+	}),
+);
+
+export default compose(withStyles(styles), withGraphQl, graphql(directorsQuery));
